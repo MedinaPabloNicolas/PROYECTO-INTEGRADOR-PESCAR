00b5ae4a90f201fd856bf3bb54c84a79dd087420
Medina Pablo <medina9898@hotmail.com>
2022-07-24 00:24:44 -0300

    menu hamburguesa terminado, falta testing resposive

--- js/acciones.js
+++ js/acciones.js
@@ -5,3 +5,13 @@ window.addEventListener('scroll', function () {
 
     barra[0].classList.toggle("scroll-abajo", window.scrollY > 0);
 });//cuando scrollea para abajo, se cambia hacia la clase abajo
+
+
+const btnMenu = document.querySelector('#btnMenu');
+const background = document.querySelector('#background');
+const menu = document.querySelector('#menu');
+btnMenu.addEventListener('click', function(){
+    menu.classList.toggle('mostrar');
+    background.classList.toggle('mostrar-background')
+})
+

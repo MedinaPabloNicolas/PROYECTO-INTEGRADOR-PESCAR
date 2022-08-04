32dff245f43ca1b0577d9b1f556bc3bf8823039a
dante <Dante_Martin@outlook.com>
2022-08-04 13:03:00 -0300

    toquetee los iconos

--- js/acciones.js
+++ js/acciones.js
@@ -9,8 +9,32 @@ window.addEventListener('scroll', function () {
 const btnMenu = document.querySelector('#btnMenu');
 const background = document.querySelector('#background');
 const menu = document.querySelector('#menu');
-btnMenu.addEventListener('click', function(){
+btnMenu.addEventListener('click', function () {
     menu.classList.toggle('mostrar');
-    background.classList.toggle('mostrar-background')
-})
+    background.classList.toggle('mostrar-background');
+});
 
+
+//-------------------------------------------------------------------//
+/*------aparece y desaparece el rectangulo de texto en menu.css------*/
+//-------------------------------------------------------------------//
+const buscarTexto = document.getElementById('boton-buscar-texto1');//con la constante agarro el id del icono
+buscarTexto.addEventListener('click', function () { //le digo que cuando le haga click al icono se ejecuta la funcion
+    let mostrarTextoCanasta = document.getElementById('mostrar-texto-canasta');//tomo al id 
+
+    let mostrarTextoLupa = document.getElementById('mostrar-texto-lupa');
+    mostrarTextoLupa.classList.remove('buscar--texto--abierto');
+    //cuando se activa mostrarTextoCanasta quiro buscar--texto--abierto a la lupa
+
+    mostrarTextoCanasta.classList.toggle('buscar--texto--abierto');//hago toggle por esta clase
+});
+//idem anterior
+const buscarTexto2 = document.getElementById('boton-buscar-texto2');
+buscarTexto2.addEventListener('click', function () {
+    let mostrarTextoLupa = document.getElementById('mostrar-texto-lupa');
+
+    let mostrarTextoCanasta = document.getElementById('mostrar-texto-canasta');
+    mostrarTextoCanasta.classList.remove('buscar--texto--abierto');
+
+    mostrarTextoLupa.classList.toggle('buscar--texto--abierto');
+});

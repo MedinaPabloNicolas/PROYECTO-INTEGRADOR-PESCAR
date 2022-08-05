493e5f677230d7bd67010b3ea9c43d6ae452cab5
dante <Dante_Martin@outlook.com>
2022-08-05 10:45:23 -0300

    algunos arreglos

--- js/acciones.js
+++ js/acciones.js
@@ -14,10 +14,25 @@ btnMenu.addEventListener('click', function () {
     background.classList.toggle('mostrar-background');
 });
 
+/*tiempo*/
+
+const buscarTiempo = document.getElementsByClassName('label')[0];
+buscarTiempo.addEventListener('click', function () {
+    /* let mostrarTiempo = document.getElementsByClassName("item");
+     mostrarTiempo[0].style.opacity = "1";
+     mostrarTiempo[1].style.opacity = "1";
+     mostrarTiempo[2].style.opacity = "1";
+ */
+    let pepe = document.getElementsByClassName("buscar--texto");
+    pepe[0].classList.remove("buscar--texto--abierto");
+    pepe[1].classList.remove("buscar--texto--abierto");
+});
 
 //-------------------------------------------------------------------//
 /*------aparece y desaparece el rectangulo de texto en menu.css------*/
 //-------------------------------------------------------------------//
+
+//canasta
 const buscarTexto = document.getElementById('boton-buscar-texto1');//con la constante agarro el id del icono
 buscarTexto.addEventListener('click', function () { //le digo que cuando le haga click al icono se ejecuta la funcion
     let mostrarTextoCanasta = document.getElementById('mostrar-texto-canasta');//tomo al id 
@@ -26,8 +41,10 @@ buscarTexto.addEventListener('click', function () { //le digo que cuando le haga
     mostrarTextoLupa.classList.remove('buscar--texto--abierto');
     //cuando se activa mostrarTextoCanasta quiro buscar--texto--abierto a la lupa
 
+
     mostrarTextoCanasta.classList.toggle('buscar--texto--abierto');//hago toggle por esta clase
 });
+
 //idem anterior lupa
 const buscarTexto2 = document.getElementById('boton-buscar-texto2');
 buscarTexto2.addEventListener('click', function () {

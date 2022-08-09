ac56ac7f316997747e3b8f3ecb42d8609b988b48
Dante-Martin <Dante_Martin@outlook.com>
2022-08-09 11:15:06 -0300

    se restringio el tamañodel texto input

--- js/recetas.js
+++ js/recetas.js
@@ -1,7 +1,7 @@
 
 
-const recetas = [['Tostado coreano', 30, 'pan, queso, huevo, jamon,', '0002.jpg', 'https://youtu.be/_XSjpOQad9w', 'Rapida', 'Paso 1: Colocamos un poco de manteca en la sartén y el pan para que se tueste por ambos lados.Sacamos y reservamos.Paso 2:En la misma sartén con un poco de aceite hacemos una tortilla con la cebolla, la lechuga y la zanahoria bien picada junto con el huevo y salpimentado.Hacemos la tortilla de manera cuadrada para que quede bien en el sándwich.Sacamos y reservamos.Paso 3: Pasamos el jamón o la panceta por la sartén y sacamos.Ahora llega el momento de meterlo en nuestro sándwich, en una de nuestras tostadas untamos el kétchup luego ponemos la tortilla, el jamón y el queso rallado que se derretirá un poco con el calor y ya está listo para comer.'],
-['Berenjena rellena de atun', 80, 'berenjena, atun, queso, cebolla,', '0001.jpg', 'https://youtu.be/vaN-BjA4YTo', 'Salud', 'Paso 1:Cortamos 2 berenjenas por la mitad y les hacemos cortes en forma de rejilla.Las regamos con un poco de aceite de oliva  y las horneamos 45 minutos a 200º.A continuación, las vaciamos con ayuda de una cuchara y reservamos la carne.Paso 2: En una sartén con aceite de oliva, pochamos 10 minutos 1 cebolla picada con sal.Añadimos la carne de las berenjenas, 2 latas de atún escurridas, 1 cdta.de comino, 1 cdta.de pimentón dulce, sal y pimienta.Mezclamos bien y añadimos 50 ml.de tomate frito.Removemos de nuevo y cocinamos 5 minutos.Paso 3: Rellenamos las berenjenas con la mezcla queso especial para gratinar(Mozzarella) Gratinamos 5 minutos a 200º.']
+const recetas = [['tostado coreano', 30, 'pan, queso, huevo, jamon,', '0002.jpg', 'https://youtu.be/_XSjpOQad9w', 'Rapida', 'Paso 1: Colocamos un poco de manteca en la sartén y el pan para que se tueste por ambos lados.Sacamos y reservamos.Paso 2:En la misma sartén con un poco de aceite hacemos una tortilla con la cebolla, la lechuga y la zanahoria bien picada junto con el huevo y salpimentado.Hacemos la tortilla de manera cuadrada para que quede bien en el sándwich.Sacamos y reservamos.Paso 3: Pasamos el jamón o la panceta por la sartén y sacamos.Ahora llega el momento de meterlo en nuestro sándwich, en una de nuestras tostadas untamos el kétchup luego ponemos la tortilla, el jamón y el queso rallado que se derretirá un poco con el calor y ya está listo para comer.'],
+['berenjena rellena de atun', 80, 'berenjena, atun, queso, cebolla,', '0001.jpg', 'https://youtu.be/vaN-BjA4YTo', 'Salud', 'Paso 1:Cortamos 2 berenjenas por la mitad y les hacemos cortes en forma de rejilla.Las regamos con un poco de aceite de oliva  y las horneamos 45 minutos a 200º.A continuación, las vaciamos con ayuda de una cuchara y reservamos la carne.Paso 2: En una sartén con aceite de oliva, pochamos 10 minutos 1 cebolla picada con sal.Añadimos la carne de las berenjenas, 2 latas de atún escurridas, 1 cdta.de comino, 1 cdta.de pimentón dulce, sal y pimienta.Mezclamos bien y añadimos 50 ml.de tomate frito.Removemos de nuevo y cocinamos 5 minutos.Paso 3: Rellenamos las berenjenas con la mezcla queso especial para gratinar(Mozzarella) Gratinamos 5 minutos a 200º.']
 
 ];
 function busqueda() {
@@ -53,6 +53,7 @@ function tiempoElegido(t, i, n) { //tiempo, ingredientes y nombre
                     //funciona alert(receta[1]);
 
                     img.src = "img/" + receta[3];
+                    h4.style.textTransform = "capitalize"
                     h4.textContent = receta[0];
 
                     var a = document.createElement("h4");

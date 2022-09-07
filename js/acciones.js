6c4e184beaa87c55af9cc1d3789f9c3bcd733f78
Medina Pablo <medina9898@hotmail.com>
2022-09-07 16:39:38 -0300

    añadido el login

--- js/acciones.js
+++ js/acciones.js
@@ -91,4 +91,16 @@ if (pepe == undefined) {
 }
 
 
+/*LOGIN*/
 
+const $btnSignIn= document.querySelector('.sign-in-btn'),
+      $btnSignUp = document.querySelector('.sign-up-btn'),  
+      $signUp = document.querySelector('.sign-up'),
+      $signIn  = document.querySelector('.sign-in');
+
+document.addEventListener('click', e => {
+    if (e.target === $btnSignIn || e.target === $btnSignUp) {
+        $signIn.classList.toggle('active');
+        $signUp.classList.toggle('active')
+    }
+});
